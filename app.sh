776554107b7bea1a3f4d939a442197b82388ed30
Amit Saxena <amit83.saxena@gmail.com>
2017-06-23 03:41:27 +0530

    Add placement strategy for service creation.

--- app.sh
+++ app.sh
@@ -88,6 +88,7 @@ then
     --task-definition $APP_NAME:$TASK_DEF_VER \
     --role ecsServiceRole \
     --load-balancers targetGroupArn=$TARGET_GRP_ARN,containerName=$APP_NAME,containerPort=80 \
+    --placement-strategy type="spread",field="attribute:ecs.availability-zone" type="binpack",field="cpu" \
     --desired-count 1 >> /dev/null 2>&1
   echo ... created service: $APP_NAME
 
