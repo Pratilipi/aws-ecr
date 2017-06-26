6ad85f7f06798782fc959d05be723cd61844a51a
Prashant Gupta <antshpra@gmail.com>
2017-06-26 08:41:16 +0000

    Update in server.sh

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.post('/*', function (req, res) {
   console.log(`Request received on ${req.path}`);
   console.log(req.body.repository.name);
   var appName = req.body.repository.name;
+
+  if( ($STAGE == 'devo' && req.body.ref != 'refs/heads/devo')
+      || ($STAGE == 'gamma' && req.body.ref != 'refs/heads/gamma')
+      || ($STAGE == 'prod' && req.body.ref != 'refs/heads/master') ) {
+    res.send(`No deployment in ${STAGE} for a commit in ${req.body.ref.substr(11)} branch.`);
+    return;
+  }
+
   if(appName.startsWith('ecs-'))
     appName = appName.substr( 4 );
   var appVersion = Math.round(new Date().getTime() / 1000 / 60);
