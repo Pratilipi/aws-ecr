b2e5d86e13c63c7d639d6de8c0178390ca92ddec
Rajat Agarwal <rajat12a@gmail.com>
2017-07-20 15:37:37 +0530

    Changes in app.sh

--- app.sh
+++ app.sh
@@ -1,54 +1,80 @@
+echo "...***==> Running bash app.sh $1 $2 $3 $4 $5"
+
 COMMAND=$1
 REALM=$2
 STAGE=$3
 APP_NAME=$4
 APP_VERSION=$5
 
-# TODO: Validate REALM as well
-if [ "$COMMAND" != "create" -a "$COMMAND" != "build" -a "$COMMAND" != "run" -a "$COMMAND" != "push" -a "$COMMAND" != "update" -a "$COMMAND" != "delete" ] || [ "$STAGE" != "devo" -a "$STAGE" != "gamma" -a "$STAGE" != "prod" ] || [ "$APP_NAME" == "" ] || [ "$APP_VERSION" == "" ]
+if [ "$COMMAND" != "create" -a "$COMMAND" != "build" -a "$COMMAND" != "run" -a "$COMMAND" != "push" -a "$COMMAND" != "update" -a "$COMMAND" != "delete" ] || [ "$REALM" != "product" -a "$REALM" != "growth" ] || [ "$STAGE" != "devo" -a "$STAGE" != "gamma" -a "$STAGE" != "prod" ] || [ "$APP_NAME" == "" ] || [ "$APP_VERSION" == "" ]
 then
   echo "syntax: bash app.sh <command> <realm> <stage> <app-name> <app-version>"
-  exit 0
+  exit 1
 fi
 
 
-# TODO: Check for ecr-task-def.raw as well
-if [ ! -f "Dockerfile.raw" ]; then
+if [ ! -f "Dockerfile.raw" ]
+then
   echo "Could not find Dockerfile.raw !"
-  exit 0
+  exit 1
 fi
 
+if [ ! -f "ecr-task-def.raw" ]
+then
+  echo "Could not find Dockerfile.raw !"
+  exit 1
+fi
 
-# LB_LISTNERs for Growth devo/gamma/prod
-# arn:aws:elasticloadbalancing:ap-southeast-1:381780986962:listener/app/devo-lb-pvt/9063c6c4e264ea17/b144bca497a9a1aa
-# arn:aws:elasticloadbalancing:ap-southeast-1:370531249777:listener/app/gamma-lb-pvt/98bfeb8d67ee2d26/e0d977e1084f2f2a
-# arn:aws:elasticloadbalancing:ap-southeast-1:370531249777:listener/app/prod-lb-pvt/bfbfa36e82445261/3e3a1b93ec7d49e1 
+if [ $REALM == "growth" ]
+then
+  PREFIX="gr-"
+  if [ $STAGE == "devo" ]
+  then
+    LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:381780986962:listener/app/devo-lb-pvt/9063c6c4e264ea17/b144bca497a9a1aa"
+  elif [ $STAGE == "gamma" ]
+  then
+    LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:370531249777:listener/app/gamma-lb-pvt/98bfeb8d67ee2d26/e0d977e1084f2f2a"
+  elif [ $STAGE == "prod" ]
+  then
+    LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:370531249777:listener/app/prod-lb-pvt/bfbfa36e82445261/3e3a1b93ec7d49e1"
+  fi
+else
+  PREFIX=""
+  if [ $STAGE == "devo" ]
+  then
+    LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:381780986962:listener/app/devo-lb-pvt/9063c6c4e264ea17/33322206f52c31c4"
+  elif [ $STAGE == "gamma" ]
+  then
+    LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:370531249777:listener/app/gamma-lb-pvt/98bfeb8d67ee2d26/a854c15563502db0"
+  elif [ $STAGE == "prod" ]
+  then
+    LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:370531249777:listener/app/prod-lb-pvt/bfbfa36e82445261/0104e43e491b57f8"
+  fi
+fi
+
 if [ $STAGE == "devo" ]
 then
   AWS_PROJ_ID="381780986962"
   VPC_ID="vpc-662a5602"
-  LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:381780986962:listener/app/devo-lb-pvt/9063c6c4e264ea17/33322206f52c31c4"
   SNS_RESOURCE="arn:aws:sns:ap-southeast-1:381780986962:devo-ecs-asg-sns"
   AUTO_SCALING_IAM_ROLE="arn:aws:iam::381780986962:role/autoscaling_ecs"
 elif [ $STAGE == "gamma" ]
 then
   AWS_PROJ_ID="370531249777"
   VPC_ID="vpc-c13c7da5"
-  LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:370531249777:listener/app/gamma-lb-pvt/98bfeb8d67ee2d26/a854c15563502db0"
   SNS_RESOURCE="arn:aws:sns:ap-southeast-1:370531249777:gamma-ecs-asg-sns"
   AUTO_SCALING_IAM_ROLE="arn:aws:iam::370531249777:role/ecsAutoscaleRole"
 elif [ $STAGE == "prod" ]
 then
   AWS_PROJ_ID="370531249777"
   VPC_ID="vpc-c13c7da5"
-  LB_LISTNER="arn:aws:elasticloadbalancing:ap-southeast-1:370531249777:listener/app/prod-lb-pvt/bfbfa36e82445261/0104e43e491b57f8"
   SNS_RESOURCE="arn:aws:sns:ap-southeast-1:370531249777:prod-ecs-asg-sns"
   AUTO_SCALING_IAM_ROLE="arn:aws:iam::370531249777:role/ecsAutoscaleRole"
 fi
 
 # TODO: Error out if project id != $AWS_PROJ_ID
 
-ECR_REPO=$AWS_PROJ_ID.dkr.ecr.ap-southeast-1.amazonaws.com/$STAGE
+ECR_REPO=$AWS_PROJ_ID.dkr.ecr.ap-southeast-1.amazonaws.com/$PREFIX$STAGE
 ECR_IMAGE=$ECR_REPO/$APP_NAME:$APP_VERSION
 
 
@@ -63,19 +89,20 @@ then
     > Dockerfile
   cat ecr-task-def.raw \
     | sed "s#\$STAGE#$STAGE#g" \
+    | sed "s#\$PREFIX#$PREFIX#g" \
     | sed "s#\$DOCKER_REPO#$ECR_REPO#g" \
     | sed "s#\$APP_NAME#$APP_NAME#g" \
     | sed "s#\$APP_VERSION#$APP_VERSION#g" \
     > ecr-task-def.json
 
-  aws ecr create-repository --repository-name $STAGE/$APP_NAME >> /dev/null 2>&1
-  echo ... create ecr repository: $STAGE/$APP_NAME
+  aws ecr create-repository --repository-name $PREFIX$STAGE/$APP_NAME >> /dev/null 2>&1
+  echo ... create ecr repository: $PREFIX$STAGE/$APP_NAME
 
   echo ... started creating target group
   echo AWS Response:
   echo "****************************************************************"
   TARGET_GRP=$(aws elbv2 create-target-group \
-    --name ecs-$STAGE-$APP_NAME-tg \
+    --name ecs-$PREFIX$STAGE-$APP_NAME-tg \
     --protocol HTTP \
     --port 80 \
     --vpc-id $VPC_ID \
@@ -117,7 +144,7 @@ then
   echo AWS response:
   echo "****************************************************************"
   aws ecs create-service \
-    --cluster $STAGE-ecs \
+    --cluster $PREFIX$STAGE-ecs \
     --service-name $APP_NAME \
     --task-definition $APP_NAME:$TASK_DEF_VER \
     --role ecsServiceRole \
@@ -128,7 +155,7 @@ then
   echo ... created service: $APP_NAME
 
   aws application-autoscaling register-scalable-target \
-    --resource-id service/$STAGE-ecs/$APP_NAME \
+    --resource-id service/$PREFIX$STAGE-ecs/$APP_NAME \
     --service-namespace ecs \
     --scalable-dimension ecs:service:DesiredCount \
     --min-capacity 2 \
@@ -136,16 +163,16 @@ then
     --role-arn $AUTO_SCALING_IAM_ROLE >> /dev/null 2>&1
 
   SCALING_POLICY_ARN=$(aws application-autoscaling put-scaling-policy \
-    --policy-name ecs-$STAGE-$APP_NAME-scaleup-cpu \
+    --policy-name ecs-$PREFIX$STAGE-$APP_NAME-scaleup-cpu \
     --service-namespace ecs \
-    --resource-id service/$STAGE-ecs/$APP_NAME \
+    --resource-id service/$PREFIX$STAGE-ecs/$APP_NAME \
     --scalable-dimension ecs:service:DesiredCount \
     --policy-type StepScaling \
     --step-scaling-policy-configuration "AdjustmentType=ChangeInCapacity,StepAdjustments=[{MetricIntervalLowerBound=0.0,ScalingAdjustment=1}],Cooldown=300,MetricAggregationType=Average" \
     | jq -r '.PolicyARN')
 
   aws cloudwatch put-metric-alarm \
-    --alarm-name ecs-$STAGE-$APP_NAME-cpu80-hi \
+    --alarm-name ecs-$PREFIX$STAGE-$APP_NAME-cpu80-hi \
     --alarm-description "ECS CPU utilization for $APP_NAME service is greater than 80% for 2 minutes" \
     --metric-name CPUUtilization \
     --namespace AWS/ECS \
@@ -153,22 +180,22 @@ then
     --period 60 \
     --threshold 80 \
     --comparison-operator GreaterThanThreshold \
-    --dimensions Name="ServiceName",Value="$APP_NAME" Name="ClusterName",Value="$STAGE-ecs" \
+    --dimensions Name="ServiceName",Value="$APP_NAME" Name="ClusterName",Value="$PREFIX$STAGE-ecs" \
     --evaluation-periods 2 \
     --treat-missing-data missing \
     --alarm-actions $SNS_RESOURCE $SCALING_POLICY_ARN >> /dev/null 2>&1
 
   SCALING_POLICY_ARN=$(aws application-autoscaling put-scaling-policy \
-    --policy-name ecs-$STAGE-$APP_NAME-scaledown-cpu \
+    --policy-name ecs-$PREFIX$STAGE-$APP_NAME-scaledown-cpu \
     --service-namespace ecs \
-    --resource-id service/$STAGE-ecs/$APP_NAME \
+    --resource-id service/$PREFIX$STAGE-ecs/$APP_NAME \
     --scalable-dimension ecs:service:DesiredCount \
     --policy-type StepScaling \
     --step-scaling-policy-configuration "AdjustmentType=ChangeInCapacity,StepAdjustments=[{MetricIntervalUpperBound=0.0,ScalingAdjustment=-1}],Cooldown=300,MetricAggregationType=Average" \
     | jq -r '.PolicyARN')
 
   aws cloudwatch put-metric-alarm \
-    --alarm-name ecs-$STAGE-$APP_NAME-cpu40-lo \
+    --alarm-name ecs-$PREFIX$STAGE-$APP_NAME-cpu40-lo \
     --alarm-description "ECS CPU utilization for $APP_NAME service is less than 40% for 10 minutes" \
     --metric-name CPUUtilization \
     --namespace AWS/ECS \
@@ -176,22 +203,22 @@ then
     --period 60 \
     --threshold 40 \
     --comparison-operator LessThanThreshold \
-    --dimensions Name="ServiceName",Value="$APP_NAME" Name="ClusterName",Value="$STAGE-ecs" \
+    --dimensions Name="ServiceName",Value="$APP_NAME" Name="ClusterName",Value="$PREFIX$STAGE-ecs" \
     --evaluation-periods 10 \
     --treat-missing-data missing \
     --alarm-actions $SNS_RESOURCE $SCALING_POLICY_ARN >> /dev/null 2>&1
 
   SCALING_POLICY_ARN=$(aws application-autoscaling put-scaling-policy \
-    --policy-name ecs-$STAGE-$APP_NAME-scaleup-burst \
+    --policy-name ecs-$PREFIX$STAGE-$APP_NAME-scaleup-burst \
     --service-namespace ecs \
-    --resource-id service/$STAGE-ecs/$APP_NAME \
+    --resource-id service/$PREFIX$STAGE-ecs/$APP_NAME \
     --scalable-dimension ecs:service:DesiredCount \
     --policy-type StepScaling \
     --step-scaling-policy-configuration "AdjustmentType=ChangeInCapacity,StepAdjustments=[{MetricIntervalLowerBound=0.0,ScalingAdjustment=10}],Cooldown=300,MetricAggregationType=Average" \
     | jq -r '.PolicyARN')
 
   aws cloudwatch put-metric-alarm \
-    --alarm-name ecs-$STAGE-$APP_NAME-cpu100-hi \
+    --alarm-name ecs-$PREFIX$STAGE-$APP_NAME-cpu100-hi \
     --alarm-description "ECS CPU utilization for $APP_NAME service is 100% for 15 minutes" \
     --metric-name CPUUtilization \
     --namespace AWS/ECS \
@@ -199,22 +226,22 @@ then
     --period 60 \
     --threshold 100 \
     --comparison-operator GreaterThanOrEqualToThreshold \
-    --dimensions Name="ServiceName",Value="$APP_NAME" Name="ClusterName",Value="$STAGE-ecs" \
+    --dimensions Name="ServiceName",Value="$APP_NAME" Name="ClusterName",Value="$PREFIX$STAGE-ecs" \
     --evaluation-periods 15 \
     --treat-missing-data missing \
     --alarm-actions $SNS_RESOURCE $SCALING_POLICY_ARN >> /dev/null 2>&1
 
   SCALING_POLICY_ARN=$(aws application-autoscaling put-scaling-policy \
-    --policy-name ecs-$STAGE-$APP_NAME-scaleup-mem \
+    --policy-name ecs-$PREFIX$STAGE-$APP_NAME-scaleup-mem \
     --service-namespace ecs \
-    --resource-id service/$STAGE-ecs/$APP_NAME \
+    --resource-id service/$PREFIX$STAGE-ecs/$APP_NAME \
     --scalable-dimension ecs:service:DesiredCount \
     --policy-type StepScaling \
     --step-scaling-policy-configuration "AdjustmentType=ChangeInCapacity,StepAdjustments=[{MetricIntervalLowerBound=0.0,ScalingAdjustment=1}],Cooldown=300,MetricAggregationType=Average" \
     | jq -r '.PolicyARN')
 
   aws cloudwatch put-metric-alarm \
-    --alarm-name ecs-$STAGE-$APP_NAME-memory90-hi \
+    --alarm-name ecs-$PREFIX$STAGE-$APP_NAME-memory90-hi \
     --alarm-description "ECS memory utilization for $APP_NAME service is more than 90% for 5 minutes" \
     --metric-name MemoryUtilization \
     --namespace AWS/ECS \
@@ -222,7 +249,7 @@ then
     --period 60 \
     --threshold 90 \
     --comparison-operator GreaterThanThreshold \
-    --dimensions Name="ServiceName",Value="$APP_NAME" Name="ClusterName",Value="$STAGE-ecs" \
+    --dimensions Name="ServiceName",Value="$APP_NAME" Name="ClusterName",Value="$PREFIX$STAGE-ecs" \
     --evaluation-periods 5 \
     --treat-missing-data missing \
     --alarm-actions $SNS_RESOURCE $SCALING_POLICY_ARN >> /dev/null 2>&1
@@ -263,6 +290,7 @@ then
     > Dockerfile
   cat ecr-task-def.raw \
     | sed "s#\$STAGE#$STAGE#g" \
+    | sed "s#\$PREFIX#$PREFIX#g" \
     | sed "s#\$DOCKER_REPO#$ECR_REPO#g" \
     | sed "s#\$APP_NAME#$APP_NAME#g" \
     | sed "s#\$APP_VERSION#$APP_VERSION#g" \
@@ -284,6 +312,7 @@ then
     > Dockerfile
   cat ecr-task-def.raw \
     | sed "s#\$STAGE#$STAGE#g" \
+    | sed "s#\$PREFIX#$PREFIX#g" \
     | sed "s#\$DOCKER_REPO#$ECR_REPO#g" \
     | sed "s#\$APP_NAME#$APP_NAME#g" \
     | sed "s#\$APP_VERSION#$APP_VERSION#g" \
@@ -294,7 +323,7 @@ then
   docker push $ECR_IMAGE
   TASK_DEF_VER=$(aws ecs register-task-definition --cli-input-json file://ecr-task-def.json | jq -r '.taskDefinition.revision')
   aws ecs update-service \
-    --cluster $STAGE-ecs \
+    --cluster $PREFIX$STAGE-ecs \
     --service $APP_NAME \
     --task-definition $APP_NAME:$TASK_DEF_VER
   rm Dockerfile
@@ -303,7 +332,9 @@ then
 elif [ $COMMAND == "delete" ]
 then
 
-  aws ecs update-service --cluster pratilipi-$STAGE-ecs --service $APP_NAME --desired-count 0
-  aws ecs delete-service --cluster pratilipi-$STAGE-ecs --service $APP_NAME
+  aws ecs update-service --cluster pratilipi-$PREFIX$STAGE-ecs --service $APP_NAME --desired-count 0
+  aws ecs delete-service --cluster pratilipi-$PREFIX$STAGE-ecs --service $APP_NAME
 
 fi
+
+echo "...***==> app.sh $1 $2 $3 $4 $5 SUCCESS"
