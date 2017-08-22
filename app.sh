493fd8599c38b26fad69536bbd9b982d0b0ae0a6
Prashant Gupta <antshpra@gmail.com>
2017-08-22 13:19:34 +0000

    Minor update in app.sh

--- app.sh
+++ app.sh
@@ -223,7 +223,7 @@ create_service()
     --task-definition $APP_NAME:$TASK_DEF_VER \
     --role ecsServiceRole \
     --load-balancers targetGroupArn=$TARGET_GRP_ARN,containerName=$APP_NAME,containerPort=80 \
-    --placement-strategy type="spread",field="attribute:ecs.availability-zone" type="binpack",field="cpu" \
+    --placement-strategy type="spread",field="instanceId" \
     --desired-count 1
   echo "****************************************************************"
   echo ... created service: $APP_NAME
