8b475ec7999e5d00ffb664dd160effcab5408b07
techpool <suryadeep10@gmail.com>
2017-09-05 16:30:55 +0530

    [GROWTH] traffic-splitter repo whitelisted

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const commands = [
 
 function checkServiceWhitelist( appName )
 {
-  var whitelist = [ "ecs", "auth", "author", "image", "pag", "page", "pratilipi", "recommendation", "search", "user-activity", "web", "hello-python", "mini", "follow", "social-connect" ];
+  var whitelist = [ "ecs", "auth", "author", "image", "pag", "page", "pratilipi", "recommendation", "search", "user-activity", "web", "hello-python", "mini", "follow", "social-connect", "traffic-splitter" ];
   if( STAGE === "prod" && REALM === "product" ) {
     whitelist.push( "datastore-util" );
   }
