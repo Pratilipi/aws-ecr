b60417f5bbc112e33f7b12509a4e00cead8392e8
Rajat Agarwal <rajat12a@gmail.com>
2017-09-08 17:37:30 +0530

    Removed hello-python from whitelist

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const commands = [
 
 function checkServiceWhitelist( appName )
 {
-  var whitelist = [ "ecs", "auth", "author", "image", "pag", "page", "pratilipi", "recommendation", "search", "user-activity", "web", "hello-python", "mini", "follow", "category-datasync" ];
+  var whitelist = [ "ecs", "auth", "author", "image", "pag", "page", "pratilipi", "recommendation", "search", "user-activity", "web", "mini", "follow", "category-datasync" ];
   if( STAGE === "prod" && REALM === "product" ) {
     whitelist.push( "datastore-util" );
   }
