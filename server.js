66629ce63d02793661b79dcf9013b4af5fe5d8b8
techpool <suryadeep10@gmail.com>
2017-09-29 12:11:03 +0530

    [GROWTH] Changed scrapper to scraper

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const commands = [
 
 function checkServiceWhitelist( appName )
 {
-  var whitelist = [ "ecs", "auth", "author", "image", "pag", "page", "pratilipi", "recommendation", "search", "user-activity", "web", "hello-python", "mini", "follow", "social-connect", "traffic-splitter", "pwg", "blog-scrapper" ];
+  var whitelist = [ "ecs", "auth", "author", "image", "pag", "page", "pratilipi", "recommendation", "search", "user-activity", "web", "hello-python", "mini", "follow", "social-connect", "traffic-splitter", "pwg", "blog-scraper" ];
   if( STAGE === "prod" && REALM === "product" ) {
     whitelist.push( "datastore-util" );
   }
