df801401fd965887c35a0a0f21c7cff969229277
gauri <gauri@pratilipi.com>
2017-12-02 05:28:46 +0530

    new nginx version supporting threadpool

--- custom-image.sh
+++ custom-image.sh
@@ -5,7 +5,8 @@ cd custom-image
 
 # bash build-image.sh $REALM $STAGE node-imagemagick 8.3.0
 # bash build-image.sh $REALM $STAGE ubuntu-nginx 1.0
-bash build-image.sh $REALM $STAGE ubuntu-nginx-sql 1.0
+bash build-image.sh $REALM $STAGE ubuntu-nginx-threadpool 1.0
+# bash build-image.sh $REALM $STAGE ubuntu-nginx-sql 1.0
 # bash build-image.sh $REALM $STAGE java-gradle 2.2.1
 
 cd ..
