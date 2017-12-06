001b9f9ec77e3a65693e246bf138109c8b08f2c8
Rajat Agarwal <rajat12a@gmail.com>
2017-12-06 17:44:38 +0530

    [CHANGE] base image trigger

--- custom-image.sh
+++ custom-image.sh
@@ -5,7 +5,7 @@ cd custom-image
 
 # bash build-image.sh $REALM $STAGE node-imagemagick 8.3.0
 # bash build-image.sh $REALM $STAGE node-sails 1.0
-# bash build-image.sh $REALM $STAGE ubuntu-nginx 1.0
+bash build-image.sh $REALM $STAGE ubuntu-nginx 1.0
 # bash build-image.sh $REALM $STAGE ubuntu-nginx 2.0
 # bash build-image.sh $REALM $STAGE ubuntu-nginx-sql 1.0
 # bash build-image.sh $REALM $STAGE java-gradle 2.2.1
