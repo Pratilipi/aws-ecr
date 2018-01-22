9944cbfb2f84a6218c859d4cf8792a429a0d5061
Rajat Agarwal <rajat12a@gmail.com>
2018-01-22 22:01:13 +0530

    [CHANGE] stopping build of base/custom images

--- custom-image.sh
+++ custom-image.sh
@@ -3,11 +3,11 @@ STAGE=$2
 
 cd custom-image
 
-# bash build-image.sh $REALM $STAGE node-imagemagick 8.3.0
-bash build-image.sh $REALM $STAGE node-imagemagick carbon
-# bash build-image.sh $REALM $STAGE node-sails 1.0
-# bash build-image.sh $REALM $STAGE node-sails 8.3.0
-bash build-image.sh $REALM $STAGE node-sails carbon
+# bash build-image.sh $REALM $STAGE node-imagemagick 8.3.0 #deprecated
+# bash build-image.sh $REALM $STAGE node-imagemagick carbon
+# bash build-image.sh $REALM $STAGE node-sails 1.0 #deprecated
+# bash build-image.sh $REALM $STAGE node-sails 8.3.0 #deprecated
+# bash build-image.sh $REALM $STAGE node-sails carbon
 # bash build-image.sh $REALM $STAGE ubuntu-nginx 1.0 #change raw file to 14.04
 # bash build-image.sh $REALM $STAGE ubuntu-nginx 2.0 #change raw file to 16.04
 # bash build-image.sh $REALM $STAGE ubuntu-nginx-sql 1.0
